d885ec12ff15751f2554eea4fb0ec612f5eb5da2
little-brother <lb.im@ya.ru>
2016-10-25 02:56:36 +0300

    initial

---
+++ README.md
@@ -0,0 +1,61 @@
+# tcp-crypto
+============
+Provide encrypted point-to-point channel between server and client via tcp.
+Server stores unsended data inside special folder while client is disconnect.
+
+## Install with [npm](npmjs.org)
+``` bash
+npm i tcp-crypto --save
+```
+
+## Usage
+### Server side
+``` javascript
+'use strict'
+const Server = require('tcp-crypto').Server;
+
+let server = new Server({
+	"port": 3000,
+	"cipher": "aes192",
+	"password": "secret",
+	"dir": "./outbox"	
+});
+
+server.on('connection', function () {
+	console.log('connect');
+	server.send('your-server-event', 'Hello from server');
+});
+server.on('disconnection', () => console.log('disconnect'));
+server.on('error', (err) => console.log(err));
+server.on('send', (msg) => null);
+server.on('your-client-event', (data) => console.log(data));
+
+// send console input
+let stdin = process.openStdin();
+stdin.addListener('data', (data) => server.send('your-server-event', data.toString()));
+```
+### Client side
+``` javascript
+'use strict'
+const Client = require('tcp-crypto').Client;
+
+let client = new Client({
+	"port": 3000,
+	"cipher": "aes192",
+	"password": "secret",
+	"reconnect-delay": 10
+});
+
+client.on('connect', function () {
+	console.log('Connect');	
+	client.send('your-client-event', 'Hello from client');	
+});
+client.on('disconnect', () => console.log('Disconnect'));	
+server.on('send', (msg) => null);
+client.on('error', (err) => null);
+client.on('your-server-event', (data) => console.log(data));
+
+// send console input
+let stdin = process.openStdin();
+stdin.addListener('data', (data) => client.send('your-client-event', data.toString()));
+```
---
+++ tcp-crypto.js
@@ -0,0 +1,227 @@
+'use strict';
+const net = require('net');
+const fs = require('fs');
+const crypto = require('crypto');
+const EventEmitter = require('events');
+const util = require('util');
+
+function encode (text, cipher, password) { 
+	let c = crypto.createCipher(cipher, password); 
+	return c.update(text, 'utf8', 'base64') + c.final('base64'); 
+}
+
+function decode (data, cipher, password) { 
+	let c = crypto.createDecipher(cipher, password); 
+	return c.update(data, 'base64', 'utf8') + c.final('utf8'); 
+}
+
+function genId() {	
+	return (new Date()).getTime() + '-' + Math.floor(Math.random() * 10000);
+}
+
+// opts = {host, port, cipher, password}
+function Client (opts) {
+	this.id = opts.id;
+	this.opts = opts;
+	this.send = send; 
+	this.isConnected = false;
+	let client = this;
+
+	let queue = [];
+	let callbacks = {};
+	let isBusy = true;
+	let socket;
+
+	connect(opts);
+
+	function send (event, data, callback) {
+		if (!client.isConnected && callback)
+			return callback(new Error('CONNECTION'));
+
+		queue.push({event, data, callback});
+		next();
+	}
+
+	function next() {
+		if (isBusy)
+			return;
+		
+		isBusy = true;
+
+		if (queue.length == 0) 
+			return (isBusy = false);
+
+		if (socket) {
+			let msg = {
+				id: genId(),
+				event: queue[0].event, 
+				data: queue[0].data
+			}
+
+			socket.write(encode(JSON.stringify(msg), opts.cipher, opts.password) + '|', function (err) {
+				if (err)
+					return socket.emit('error', err);
+
+				if (queue[0].callback)		
+					callbacks[msg.id] = queue[0].callback;
+
+				client.emit('send', queue[0]);
+				queue.shift(); 
+				isBusy = false;
+				next();
+			});
+		}	
+	}
+	
+	function connect (opts) {
+		socket = net.connect({host: opts.host, port: opts.port});
+		queue = [];
+		callbacks = {};
+		isBusy = true;
+
+		socket.on('connect', function () {
+			isBusy = false;
+			client.isConnected = true;
+			client.emit('connect', opts.id);
+		});
+
+		let buffer = '';
+		socket.on('data', function(chunk) {
+			buffer += chunk;
+			let msgs = buffer.split('|');
+			buffer = msgs.pop();
+
+			msgs.forEach(function(packet){
+
+				let msg;
+				try { 
+					msg = JSON.parse(decode(packet, opts.cipher, opts.password));
+				} catch(err) { 
+					return socket.emit('error', err);
+				}
+
+				client.emit('receive', msg);
+	
+				if (msg.id in callbacks) {	
+					callbacks[msg.id](msg.data);
+					delete callbacks[msg.id];			
+					return;
+				}
+				
+				if (msg.event != 'ACK') {
+					client.emit(msg.event, msg.data);
+					send('ACK', {id: msg.id, event: msg.event});
+					return;
+				}
+			});
+		});	
+
+		socket.on('close', () => {
+			setTimeout(connect, opts['reconnect-delay'] * 1000 || 30000, opts);
+			if (client.isConnected) {
+				client.isConnected = false;
+				client.emit('disconnect', opts.id);	
+			}
+			client.isConnected = false;
+		});
+
+		socket.on('error', (err) => client.emit('error', err));		
+	}	
+}
+util.inherits(Client, EventEmitter);
+
+// opts = {port, cipher, password, dir}
+function Server (opts) { 
+	let server = this;
+	server.send = send;
+	let isBusy = true;	
+
+	let queue = fs.readdirSync(opts.dir)
+		.map((f) => { return {id: f.split('.')[0], event: f.split('.')[1], data: fs.readFileSync(`${opts.dir}/${f}`)};})
+		.map((msg) => { 
+			try { 
+				msg.data = JSON.parse(msg.data);
+			} catch (err) { 
+				server.emit('error', 'Error on parse ${msg.id}: ' + err.message); 
+				msg.data = ''
+			}; 
+			return msg;
+		})
+		.filter((msg) => !!msg.data);	
+	queue.add = function(msg) {
+		this.push(msg);
+		fs.writeFileSync(`${opts.dir}/${msg.id}.${msg.event}`, JSON.stringify(msg.data), {'flags': 'a'});
+	}
+
+	function send(event, data, id) {
+		queue.add({event, data, id: id || genId()});
+		if (server.next)
+			server.next();
+	}
+
+	let netServer = net.createServer(function(socket){
+		isBusy = false;
+		server.next = next;
+		server.emit('connection', opts.id);
+		next();
+		
+		function next() {
+			if (isBusy)
+				return;
+
+			isBusy = true;
+
+			if (queue.length == 0) 
+				return (isBusy = false);
+	
+			if (socket)
+				socket.write(encode(JSON.stringify(queue[0]), opts.cipher, opts.password) + '|', () => { 
+					server.emit('send', queue[0]);
+					queue.shift();
+					isBusy = false; 
+					next(); 
+				});
+		}
+		
+		let buffer = '';
+		socket.on('data', function(chunk) {
+			buffer += chunk;
+			if (buffer.indexOf('|') == -1)
+				return;
+	
+			let msgs = buffer.split('|');
+			buffer = msgs.pop();
+
+			msgs.forEach(function(packet){
+
+				let msg;
+				try { 
+					msg = JSON.parse(decode(packet, opts.cipher, opts.password));
+				} catch(err) { 
+					return socket.emit('error', err);
+				}	
+				server.emit('receive', msg);
+				
+				if (msg.event == 'ACK') {
+					fs.unlink(`${opts.dir}/${msg.data.id}.${msg.data.event}`, (err) => err ? console.log(err) : null);
+					return;
+				}
+		
+				server.emit(msg.event, msg.data, (result) => server.send(msg.event, result, msg.id));
+			});	
+		});
+	
+		socket.on('close', () => { 
+			server.emit('disconnection', opts.id) 
+			socket = undefined; 
+		});
+	
+		socket.on('error', (err) => server.emit('error', err));
+	});
+	netServer.maxConnections = 1;
+	netServer.listen(opts.port, (err) => console.log(err ? err : 'Listen on ' + opts.port));	
+	netServer.on('close', () => process.exit(1));
+}
+util.inherits(Server, EventEmitter);
+
+module.exports = {Client, Server};
